cea180699186b9db87da616c236037182a274477
MonkSangha <monksangharaj@gmail.com>
2024-01-18 12:59:34 +0530

    FAQ Creation

--- devignite/app/FAQ/page.js
+++ devignite/app/FAQ/page.js
@@ -35,33 +35,42 @@ function TracksPage() {
       <div className="heading">FAQ</div>
       <div className='a-main'>
         <TrackAccordion
-          title="Generative AI and Machine Learning"
-          content="This track focuses on the exciting field of Generative AI and Machine Learning, exploring techniques that allow computers to learn and generate new content, such as images, text, or music."
+          title="Team Size?"
+          content="3-4"
+        />
+        <TrackAccordion
+          title="Registration costs??"
+          content="Nada."
+        />
+        <TrackAccordion
+          title="Is This Hackathon Biginner Friendly?"
+          content="Yes, this hackathon is beginner friendly where we cater to new comers with a great deal of attention."
         />
 
         <TrackAccordion
-          title="Blockchain"
-          content="The Blockchain track delves into the revolutionary technology behind cryptocurrencies. Learn about decentralized ledgers, smart contracts, and the potential impact of blockchain on various industries."
+          title="How will participating in Devignite benefit?"
+          content="DevIgnite will feature sessions by experienced speakers, engaging workshops, and mentors to inspire attendees in all spheres of tech. Participants can contribute to projects in diverse fields, including AI, ML, Web and App development, BlockChain, Social Cause, Cybersecurity, Cloud & DevOps. We support original ideas with free patent assistance, emphasizing scalability."
         />
 
         <TrackAccordion
-          title="Cybersecurity"
-          content="In the Cybersecurity track, participants will explore the world of digital security. Learn about the latest threats, vulnerabilities, and defensive strategies to secure systems and data from cyber attacks."
+          title="What are the eligibility criteria for participation in Devignite?"
+          content="Anyone and everyone who is a tech enthusiast and an avid learner is encouraged to participate. There are no specific eligibility criteria as such. From students to working professionals, an avid interest in engaging and contributing to the community is all that DevIgnite looks for in its participants.
+          So, if you want to explore, network, and contribute to exciting projects, you are welcome."
         />
 
         <TrackAccordion
-          title="Cloud and DevOps"
-          content="The Cloud and DevOps track covers cloud computing and development operations. Discover how to deploy, manage, and scale applications in the cloud while incorporating DevOps practices for efficient development workflows."
+          title="When and Where is the event happening?"
+          content="DevIgnite is tentatively scheduled to be hosted in the first week of March. The venue is Smt. Indira Gandhi College of Engineering Ghansoli, Navi Mumbai."
         />
 
         <TrackAccordion
-          title="Web and App Development"
-          content="Participants in this track will dive into the realm of web and app development. Learn about front-end and back-end technologies, frameworks, and best practices for creating modern and responsive applications."
+          title="How is the Cost to Participate?"
+          content="DevIgnite always will be a free event. You won't need to pay anything to participate in this hackathon."
         />
 
         <TrackAccordion
-          title="Open Track : Build For A Good Cause!"
-          content="The Open Track invites participants to leverage their skills for a good cause. Collaborate on projects that address social, environmental, or community challenges. Make a positive impact through technology."
+          title="Can we Leave the campus After Hackathon starts?"
+          content="The hackathon is scheduled to last for 24 hours, during which participants are encouraged to stay on campus for an uninterrupted experience. However, if participants choose to go outside the college campus, they do so at their own discretion and assume responsibility for any consequences. Accommodation and meal facilities will be provided on campus, ensuring convenience for those who opt to stay. Team DevIgnite is fully committed to addressing any urgent requirements of participants, aiming to facilitate a smooth and enjoyable experience throughout the event."
         />
 
       </div>
